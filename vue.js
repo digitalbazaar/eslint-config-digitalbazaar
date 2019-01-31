c00ef0f55cba1f5184c7bfa61509a68185b9f3c7
Andrew Jones <ajones@digitalbazaar.com>
2019-01-31 13:56:42 -0500

    Add a special rule set for vue.

---
+++ vue.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "env": {
+    "browser": true,
+  },
+  "extends": ["./index.js", "plugin:vue/recommended"],
+  "rules": {
+    "vue/mustache-interpolation-spacing": "never"
+  }
+};
