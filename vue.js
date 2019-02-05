a7a77a063c83cdbe44840ea9c045d490bc569eeb
Ganesh Annan <gannan@digitalbazaar.com>
2019-02-05 16:41:21 -0500

    Update closing bracket rule.

--- vue.js
+++ vue.js
@@ -4,6 +4,10 @@ module.exports = {
   },
   "extends": ["./index.js", "plugin:vue/recommended"],
   "rules": {
-    "vue/mustache-interpolation-spacing": "never"
+    "vue/mustache-interpolation-spacing": "never",
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }]
   }
 };
