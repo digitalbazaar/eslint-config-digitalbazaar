6d2b1487f176ca7de715807094c337646456f70e
andrewjones <ajones@digitalbazaar.com>
2019-02-26 10:04:59 -0600

    Remove danglings commas from linter.
    Remove dangling commas from index.js
    
    Allow linter project to lint itself.
    
    Lint the vue rules.
    
    Project lint itself.
    
    Add a rule set that uses the root rules.
    
    Reduce hypocrisy in linter project.
    
    Fix 2 lines in vue rules.

--- vue.js
+++ vue.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "env": {
-    "browser": true,
+  env: {
+    browser: true
   },
-  "extends": ["./index.js", "plugin:vue/recommended"],
-  "rules": {
-    "vue/mustache-interpolation-spacing": "never",
-    "vue/html-closing-bracket-newline": ["error", {
-      "singleline": "never",
-      "multiline": "never"
+  extends: ['./index.js', 'plugin:vue/recommended'],
+  rules: {
+    'vue/mustache-interpolation-spacing': 'never',
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'never'
     }]
   }
 };
