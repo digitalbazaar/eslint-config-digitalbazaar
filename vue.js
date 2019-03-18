b48835c709b85f22a4a57c5d1549d8072fd163db
andrewjones <ajones@digitalbazaar.com>
2019-03-18 11:56:09 -0600

    Vue rules extend vue recommended rules.

--- vue.js
+++ vue.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true
   },
+  extends: ['plugin:vue/recommended'],
   rules: {
     'vue/mustache-interpolation-spacing': 'never',
     'vue/html-closing-bracket-newline': ['error', {
