1185a2e17bb43066ddd1578e4eb3732115bd28ee
andrewjones <ajones@digitalbazaar.com>
2019-03-18 10:42:59 -0600

    Remove extends base rules from jsdoc & vue.

--- vue.js
+++ vue.js
@@ -2,7 +2,6 @@ module.exports = {
   env: {
     browser: true
   },
-  extends: ['./index.js', 'plugin:vue/recommended'],
   rules: {
     'vue/mustache-interpolation-spacing': 'never',
     'vue/html-closing-bracket-newline': ['error', {
