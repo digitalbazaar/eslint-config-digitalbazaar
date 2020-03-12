0bba2f419ab57aacaca15a822e338190ff359786
Andrew Jones <ajones@digitalbazaar.com>
2020-03-12 17:26:51 -0400

    Dissalow yoda comparisons.

---
+++ test/index.js
@@ -0,0 +1,38 @@
+/* eslint no-unused-vars: 0 */
+
+/* eslint no-implicit-coercion: 2 */
+const num = 1;
+const numString = `${num}`;
+const implictConvert = `` + num;
+
+const justtemplate = `foo`;
+
+/* eslint no-useless-concat: 2 */
+
+const noConcat = 'a' + 'b';
+
+/* eslint no-invalid-regexp: 2 */
+
+const r = new RegExp('\\');
+
+/* eslint no-cond-assign: 2 */
+const a = {foo: null}
+
+if(a.foo = 'x') {
+
+}
+
+/* eslint no-dupe-else-if: 2 */
+const n = 5;
+
+if(n === 1) {
+} else if(n === 2) {
+} else if(n === 3) {
+} else if(n === 2) {
+} else if(n === 5) {
+}
+
+const color = 'foo';
+if('red' === color) {
+
+}
