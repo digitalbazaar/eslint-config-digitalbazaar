373077f4db0ebdbd823ccccbaabc4f542323a807
Andrew Jones <ajones@digitalbazaar.com>
2022-06-24 10:08:43 -0400

    Update README with .cjs extension & add further examples.

--- test/imports/index.js
+++ test/imports/index.js
@@ -1,3 +1,4 @@
+// successfully imports a default with no errors
 import d from './default.js';
 
 // this does not count as a duplicate import
@@ -7,9 +8,8 @@ import * as b from './default.js';
 // throws namespace error
 b.d();
 
-// import not found
-// also throws no duplicates
-import {notFound1} from './default.js'; 
+// throws a parse error from the duplicate export
+import {duplicate} from './duplicate.js';
 
 // throws because notFound is not exported
 import {found, notFound} from './notExported.js';
@@ -20,7 +20,13 @@ import unresolved from './unresolved.js';
 // throws no default export
 import default1 from './noDefault.js';
 
+// throws because of duplicate import
+// useful for catching cases where you might have
+// duplicate imports of the same package with different bindings/members
+import {member1} from './duplicateImport.js';
+import {member2} from './duplicateImport.js';
+
 // this just gets around linter errors with unused vars
 export const used = {
-  d, unresolved, found,
+  d, unresolved, found, duplicate, member1, member2
 };
