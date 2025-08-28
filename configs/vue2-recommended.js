735b537da6eda5ca71572de43a5e96c2c8156132
David I. Lehn <dlehn@digitalbazaar.com>
2025-08-27 22:24:33 -0400

    Remove Vue 2 support.

--- configs/vue2-recommended.js
+++
@@ -1,7 +0,0 @@
-import browser from './browser-recommended.js';
-import vue2 from './vue2.js';
-
-export default [
-  ...browser,
-  ...vue2
-];
--- templates/vue2.js
+++
@@ -1,5 +0,0 @@
-import config from 'eslint-config-digitalbazaar/vue2-recommended';
-
-export default [
-  ...config
-];
