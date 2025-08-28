1b531da796719c65c5e0cefa2cb197a04597d01b
David I. Lehn <dlehn@digitalbazaar.com>
2025-08-27 22:58:30 -0400

    Use `eslint-plugin-eslint-plugin`.

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
+import eslintPlugin from 'eslint-plugin-eslint-plugin';
 import importConfig from './configs/import.js';
 import nodeConfig from './configs/node-recommended.js';
 
 export default [
+  eslintPlugin.configs.recommended,
   ...nodeConfig,
   {
     files: ['test/import/*.js'],
