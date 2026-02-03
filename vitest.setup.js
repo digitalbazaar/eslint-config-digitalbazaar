e3ce7fc572dfb5107a50a26bc7a1235b874dbc12
David I. Lehn <dlehn@digitalbazaar.com>
2026-02-03 00:03:26 -0500

    Fix lint issue.

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,5 +1,5 @@
-import {RuleTester} from 'eslint';
 import {afterAll, describe, it} from 'vitest';
+import {RuleTester} from 'eslint';
 
 // Assign Vitest's functions to RuleTester's static properties
 RuleTester.afterAll = afterAll;
